28701952182b1c949bb9d98e5e441e76d1026fda
Han <123021973+HanTheDestroyer@users.noreply.github.com>
2023-05-18 02:09:19 +0300

    Update barnsley_fern.py

--- barnsley_fern.py
+++ barnsley_fern.py
@@ -1,8 +1,12 @@
+import sys
+
 import numpy as np
 import matplotlib.pyplot as plt
+import pygame as pg
 
-
-number_of_iterations = 250000
+number_of_iterations = 100000
+mode = 'pygame'
+# mode = 'matplotlib'
 
 
 def function_1(a, b):
@@ -45,9 +49,33 @@ for i in np.arange(1, number_of_iterations):
     function = np.random.choice(functions, p=np.array([0.01, 0.85, 0.07, 0.07]))
     x[i], y[i] = function(x[i-1], y[i-1])
 
+if mode == 'matplotlib':
+    fig = plt.figure(dpi=120, facecolor=[0.4, 0.4, 0.4])
+    ax0 = fig.add_subplot()
+    ax0.scatter(x, y, s=1, label=f'{number_of_iterations} data points', c='Green')
+    plt.legend()
+    plt.show()
 
-fig = plt.figure(dpi=120, facecolor=[0.4, 0.4, 0.4])
-ax0 = fig.add_subplot()
-ax0.scatter(x, y, s=1, label=f'{number_of_iterations} data points', c='Green')
-plt.legend()
-plt.show()
+
+if mode == 'pygame':
+    pg.init()
+    screen = pg.display.set_mode([1200, 1200])
+    clock = pg.time.Clock()
+    scale = 120
+    running = 1
+    while running:
+        if running > number_of_iterations-1:
+            running = 1
+            screen.fill([0, 0, 0])
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+                sys.exit()
+        pg.draw.circle(screen, color=[0, 255 - y[running] * 20, 0],
+                       center=[x[running] * scale + 600, 1200 - y[running] * scale], radius=1)
+        pg.display.update()
+        clock.tick()
+        running += 1
+
+    pg.quit()
